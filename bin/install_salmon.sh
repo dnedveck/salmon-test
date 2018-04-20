2a7602430c630811171b5c76091bb51ffd1ee005
Derek Nedveck <dnedveck@gmail.com>
2018-04-20 08:09:15 -0500

    made a set_env.sh, might be easier to maintain/contain

--- bin/install_salmon.sh
+++ bin/install_salmon.sh
@@ -6,6 +6,3 @@ wget https://github.com/COMBINE-lab/salmon/releases/download/v0.9.1/Salmon-0.9.1
 # untar
 tar zxvf Salmon-0.9.1_linux_x86_64.tar.gz
 
-# adding to PATH
-salmonpath=`pwd`
-echo -e "# adding Salmon binary to path\nexport PATH=$salmonpath/Salmon-latest_linux_x86_64/bin:$PATH" >> ~/.bashrc
---
+++ set_env.sh
@@ -0,0 +1,9 @@
+
+
+# Setting paths
+salmonpath=`ls -d bin/Salmon*/bin`
+
+
+# exporting the new path
+
+export PATH=$PATH:$salmonpath
